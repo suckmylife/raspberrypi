24da49c8332539a88bc91657d8f8e32a6381de88
kosa <thddpfla1107@gmail.com>
2025-06-13 11:32:43 +0900

    fork

---
+++ 0613/fork.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+
+static int g_var = 1;
+char str[] = "PID";
+
+int main(int arg, char **argv)
+{
+    int var;
+    pid_t pid;
+    var = 92;
+
+    if((pid = fork())<0){
+        perror("[ERROR] : fork()");
+    }
+    else if(pid == 0){
+        g_var++;
+        var++;
+        printf("Parent %s from parent Process(%d) : %d \n",str, getpid(),pid);
+        sleep(1);
+    }
+    printf("pid = %d, Global var = %d, var = %d \n",getpid(),g_var,var);
+    return 0;
+}
