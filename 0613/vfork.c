25bcfcf60b76dbd4c78b9841d10ad58bbed72dbe
kosa <thddpfla1107@gmail.com>
2025-06-13 12:13:17 +0900

    vfork

--- 0613/vfork.c
+++ 0613/vfork.c
@@ -8,19 +8,20 @@ int main(int arg, char **argv)
 {
     int var;
     pid_t pid;
-    var = 92;
+    var = 88;
 
-    if((pid = fork())<0){
-        perror("[ERROR] : fork()");
+    if((pid = vfork())<0){
+        perror("[ERROR] : vfork()");
     }
     else if(pid == 0){
         g_var++;
         var++;
         printf("Parent %s from Child Process(%d) : %d \n",str, getpid(),getppid());
+        printf("pid = %d, Global var = %d, var = %d \n",getpid(),g_var,var);
+        _exit(0);
     }
     else{
          printf("Child %s from Parent Process(%d) : %d \n",str, getpid(),pid);
-         sleep(1);
     }
     printf("pid = %d, Global var = %d, var = %d \n",getpid(),g_var,var);
     return 0;
