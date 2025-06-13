44228d0710319ddc00b3fa01c19aaace2ec63eb4
kosa <thddpfla1107@gmail.com>
2025-06-13 14:57:35 +0900

    posix

--- 0613/spawn.c
+++ 0613/spawn.c
@@ -8,11 +8,17 @@ int system(char *cmd)
 {
     pid_t pid;
     int status;
+    posix_spawn_file_actions_t actions;
+    posix_spawnattr_t attrs;
     char *argv[] = {"sh","-c",cmd,NULL};
 
-    posix_spawn(&pid,"/bin/sh",NULL,NULL,argv,environ);
+    posix_spawn_file_actions_init(&actions);
+    posix_spawnattr_init(&attrs);
+    posix_spawnattr_setflags(&attrs,POSIX_SPAWN_SETSCHEDULER);
 
-    waitpid(pid,&status,0);
+    posix_spawn(&pid,"/bin/sh",&actions,&attrs,argv,environ);
+
+    //waitpid(pid,&status,0);
     return status;
 }
 
