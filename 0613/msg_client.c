46ec51ed5e6c3545348927d2292328a0281b135c
kosa <thddpfla1107@gmail.com>
2025-06-13 17:40:37 +0900

    msg

---
+++ 0613/msg_client.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/msg.h>
+
+#define MSQKEY 51234
+
+struct msgbuf
+{
+    long mytype;
+    char mtext[BUFSIZ];
+};
+
+int main(int argc, char **argv)
+{
+    key_t key;
+    int rc,msqid;
+    char *msg_text = "hello world\n";
+    struct msgbuf *mb;
+    mb = (struct msgbuf*)malloc(sizeof(struct msgbuf)+strlen(msg_text));
+    
+    key = MSQKEY;
+
+    if((msqid = msgget(key,0666))<0){
+        perror("msgget()");
+        return -1;
+    }
+    mb->mytype = 1;
+    strcpy(mb->mtext,msg_text);
+    rc = msgsnd(msqid,mb,strlen(msg_text)+1,0);
+    if(rc == -1)
+    {
+        perror("msgsnd()");
+        return -1;
+    }
+    mb->mytype = 2;
+    memset(mb->mtext,0,sizeof(mb->mtext));
+    if(msgsnd(msqid,mb,sizeof(mb->mtext),0)<0){
+        perror("msgsnd()");
+        return -1;
+    }
+    
+    return 0;
+}
+
