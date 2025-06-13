15249ecbf35710bb03814556fe36955ba9afbd4a
kosa <thddpfla1107@gmail.com>
2025-06-13 15:46:49 +0900

    pipe

---
+++ 0613/pipe.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int main(int arg, char **argv)
+{
+    pid_t pid;
+    int pfd[2];
+    char line[BUFSIZ];
+    int status;
+
+    if(pipe(pfd)<0){
+        perror("pipe()");
+        return -1;
+    }
+
+    if((pid = fork())<0){
+        perror("[ERROR] : fork()");
+        return -1;
+    }
+    else if(pid == 0){
+        close(pfd[0]);
+        dup2(pfd[1],1);
+        execl("/bin/date","date",NULL);
+        close(pfd[1]);
+        _exit(127);
+    }
+    else{
+        close(pfd[1]);
+        if(read(pfd[0],line,BUFSIZ) < 0){
+            perror("read()");
+            return -1;
+        }
+        printf("%s",line);
+        close(pfd[0]);
+        waitpid(pid,&status,0);
+    }
+    return 0;
+}
