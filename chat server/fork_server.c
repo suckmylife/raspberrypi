019a097dfcb94054ef692626d4f1e01e6857b4f7
Hey <96474168+suckmylife@users.noreply.github.com>
2025-06-23 00:47:21 +0900

    Update fork_server.c

--- chat server/fork_server.c
+++ chat server/fork_server.c
@@ -11,16 +11,16 @@
 #include "deamon.h"
 
 #define TCP_PORT   5100
-#define MAX_CLIENT 32;
+#define MAX_CLIENT 32
 
 // 각 자식 프로세스(클라이언트 핸들러)의 정보를 담는 구조체
 typedef struct {
     // 자식 프로세스의 PID
     pid_t pid;             
     // 부모가 이 자식에게 메시지를 보낼 때 사용하는 파이프의 '쓰기' 끝 FD (부모 관점)
-    int child_write_fd;
+    int parent_to_child_write_fd;
     // 이 자식이 부모에게 메시지를 보낼 때, 부모가 '읽을' 파이프의 FD (부모 관점)
-    int parent_read_fd;
+    int child_to_parent_read_fd;
 } pipeInfo;
 
 int main(int argc, char **argv)
@@ -29,7 +29,6 @@ int main(int argc, char **argv)
     int csock; //클라이언트 소켓 
     socklen_t clen; //주소 구조체  길이를 저장할 변수 
     struct sockaddr_in servaddr, cliaddr;//클라이언트의 주소정보를 담을 빈그릇
-    char mesg[BUFSIZ]; //메시지 읽는거
     struct sigaction sa; //SIGIO를 쓰기 위한 안정된 비동기 핸들러
     
     daemonize(argv); //데몬화
@@ -65,12 +64,8 @@ int main(int argc, char **argv)
         int n, csock = accept(ssock,(struct sockaddr *)&cliaddr,&clen);
         //파이프 관련 초기화 
         pid_t new_pid; //부모 자식 구분자
-        int parent_pfd[2]; //부모 fd
-        int child_pfd[2]; //자식 fd 
-        
-        pipe_info[client_num].pid = new_pid;
-        pipe_info[client_num].child_write_fd  = parent_pfd[2];
-        pipe_info[client_num].parent_read_fd  = child_pfd[2];
+        int parent_pfd[2]; //부모->자식 fd
+        int child_pfd[2];  //자식->부모 fd 
         
         //디버깅용 코드
         //클라이언트의 IP 주소를 사람이 읽을 수 있는 문자열 형태로 변환하는 역할
@@ -78,41 +73,99 @@ int main(int argc, char **argv)
         syslog(LOG_INFO,"Client is connected : %s",mesg);
 
         //자식 --> 부모 파이프 생성
-        if(pipe(pipe_info[client_num].child_write_fd)<0){ 
+        if(pipe(child_pfd)<0){ 
             syslog(LOG_ERR,"No CHILD PIPE!");
             exit(1);
         }
         //부모 --> 자식 파이프 생성
-        if(pipe(pipe_info[client_num].parent_read_fd)<0){ 
+        if(pipe(parent_pfd)<0){ 
             syslog(LOG_ERR,"No PARENT PIPE!");
             exit(1);
         }
 
-        if((pipe_info[client_num].pid = fork())<0){
+        if((new_pid = fork())<0){
             syslog(LOG_ERR,"NO FORK!!");
             exit(1);
         }
-        else if(pipe_info[client_num].pid == 0){ //자식
-            close(pipe_info[client_num].child_write_fd[0]);
-            
-            close(pipe_info[client_num].child_write_fd[1]);
-        }
-        else{ //부모
-            close(pipe_info[client_num].parent_read_fd[1]);
-            
-            close(pipe_info[client_num].parent_read_fd[0]);
-            waitpid(pid,&status,0);
-        }
+        else if(new_pid == 0){ //자식 : 클라이언트에서 쓴걸 읽고 부모에게 보낸다
+            char mesg[BUFSIZ]; //메시지 읽는거
+            close(ssock); //자식은 클라이언트를 감지 하지 않아도 되니까 닫음
 
-        if((n=read(csock,mesg,BUFSIZ)) <= 0){
-            syslog(LOG_ERR,"cannot read client message");
+            //부모는 parent_pfd[1]에 쓰고, 자식은 parent_pfd[0]에서 읽음
+            //자식은 읽어야 하니까 반대를 닫는다
+            close(parent_pfd[1]); 
+            //자식은 child_pfd[1]에 쓰고, 부모는 child_pfd[0]에서 읽음
+            //자식이 써야 하니까 반대를 닫는다
+            close(child_pfd[0]);
+            while(1){
+                //클라이언트에서 메시지를 받는다
+                if((n=read(csock,mesg,BUFSIZ)) <= 0){
+                    syslog(LOG_ERR,"cannot read client message");
+                    break;
+                }
+                else{
+                    mesg[n] = '\0';
+                    syslog(LOG_INFO,"Received data : %s",mesg);
+                    if(write(child_pfd[1],mesg,n) <= 0) //클라이언트에게 받은걸 부모에게 쓴다.
+                        syslog(LOG_ERR,"cannot Write to parent");
+                }
+                //부모의 메시지를 읽는다
+                if((n=read(parent_pfd[0],mesg,BUFSIZ)) <= 0){
+                    syslog(LOG_ERR,"cannot read SERVER message");
+                    break;
+                }
+                else{
+                    mesg[n] = '\0';
+                    syslog(LOG_INFO,"Received data : %s",mesg);
+                    if(write(csock,mesg,n) <= 0) //클라이언트에게 받은걸 부모에게 쓴다.
+                        syslog(LOG_ERR,"cannot Write to client");
+                }
+            }
+            close(csock); //할일 다 했으니까 종료
+            //열어놓은 파이프 닫기
+            close(parent_pfd[0]);
+            close(child_pfd[1]); 
         }
+        else{ //부모 : 자식이 보낸걸 읽고 
+            char mesg[BUFSIZ]; //메시지 읽는거
+            close(csock); // 부모는 클라이언트 소켓을 쓰지 않으니까
+            //부모는 parent_pfd[1]에 쓰고, 자식은 parent_pfd[0]에서 읽음
+            //부모는 써야 하니까 반대를 닫는다
+            close(parent_pfd[0]); 
+            //자식은 child_pfd[1]에 쓰고, 부모는 child_pfd[0]에서 읽음
+            //부모는 읽어야 하니까 반대를 닫는다
+            close(child_pfd[1]);
+            pipe_info[client_num].pid = new_pid;
+            //부모가 자식에게 쓰고 있는 파이프의 단 할당
+            pipe_info[client_num].parent_to_child_write_fd  = parent_pfd[1];
+            //자식이 부모에게 온걸 읽을 수 있는 파이프의 단 할당
+            pipe_info[client_num].child_to_parent_read_fd  = child_pfd[0];
+            client_num++;
 
-        syslog(LOG_INFO,"Received data : %s",mesg);
-
-        if(write(csock,mesg,n) <= 0)
-            syslog(LOG_ERR,"cannot Write");
-        close(csock);
+            for(int i = 0; i<client_num; i++)
+            {
+                //자식의 메시지를 읽는다
+                if((n=read(pipe_info[i].child_to_parent_read_fd,mesg,BUFSIZ)) <= 0){
+                    syslog(LOG_ERR,"cannot read child message");
+                    break;
+                }
+                else{
+                    //다른 자식프로세스에 메시지를 보낸다.
+                    mesg[n] = '\0';
+                    for(int j = 0; j < client_num; j++){
+                        syslog(LOG_INFO,"Received data : %s",mesg);
+                        if(write(pipe_info[j].parent_to_child_write_fd,mesg,n) <= 0) 
+                            syslog(LOG_ERR,"cannot Write to child");
+                    }
+                    
+                }
+            }
+            close(ssock);
+            //열어놓은 파이프 닫기
+            close(parent_pfd[1]);
+            close(child_pfd[0]); 
+        }
+        
     }while(strncmp(mesg,"q",1));
 
     close(ssock);
