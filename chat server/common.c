47734a353576b918a385880c94ebbeee2b81a2c5
kosa <thddpfla1107@gmail.com>
2025-06-24 17:50:12 +0900

    re modi

--- chat server/common.c
+++ chat server/common.c
@@ -1,8 +1,8 @@
 #include "common.h"
 
-int client_num = 0;
-int room_num = 0;
-int child_exited_flag = 0;
-int is_write_from_client = 0;
-int is_write_from_chat_room = 0;
-int is_shutdown = 0;
+volatile sig_atomic_t is_write_from_chat_room = 0;
+volatile sig_atomic_t is_write_from_client = 0;
+volatile sig_atomic_t is_shutdown = 0;
+volatile sig_atomic_t child_exited_flag = 0;
+volatile sig_atomic_t client_num = 0;
+volatile sig_atomic_t room_num = 0;
