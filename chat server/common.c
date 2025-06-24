df41d5605f367af06727011dc3e36d63713fbc8b
kosa <thddpfla1107@gmail.com>
2025-06-24 17:52:31 +0900

    common c modi

--- chat server/common.c
+++ chat server/common.c
@@ -6,3 +6,6 @@ volatile sig_atomic_t is_shutdown = 0;
 volatile sig_atomic_t child_exited_flag = 0;
 volatile sig_atomic_t client_num = 0;
 volatile sig_atomic_t room_num = 0;
+roomInfo room_info[CHAT_ROOM];          // 실제 메모리 할당 및 초기화 (필요 시)
+pipeInfo client_pipe_info[CHAT_ROOM];
+
