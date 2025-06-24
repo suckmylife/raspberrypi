9a5d3521106263321ec0a45a3982b806b15354d1
kosa <thddpfla1107@gmail.com>
2025-06-24 19:46:07 +0900

    new

--- chat server/clientmanager.c
+++ chat server/clientmanager.c
@@ -20,6 +20,7 @@ void client_work(pid_t client_server_pid,pid_t main_server_pid, int client_sock_
             mesg[n] = '\0';
             syslog(LOG_INFO,"Received Client data : %s",mesg);
             snprintf(add_mesg, BUFSIZ, "%d:%s", client_server_pid, mesg);
+            syslog(LOG_INFO,"Received Client data : %s",add_mesg);
             if(write(client_to_main_pipe_fds[1],add_mesg,strlen(add_mesg)+1) <= 0) //클라이언트에게 받은걸 부모에게 쓴다.
                 syslog(LOG_ERR,"cannot Write to parent");
             else{
