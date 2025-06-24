a618bb7cd3283ed4ede3ae7dadacd173eda73533
kosa <thddpfla1107@gmail.com>
2025-06-24 17:32:06 +0900

    end clientmanager modi

--- chat server/clientmanager.c
+++ chat server/clientmanager.c
@@ -1,6 +1,6 @@
 #include "clientmanager.h"
 
-void client_work(pid_t client_server_pid,pid_t main_server_pid, int client_sock_fd, int client_read_pipe_fd, int client_write_pipe_fd)
+void client_work(pid_t client_server_pid,pid_t main_server_pid, int client_sock_fd, int client_read_pipe_fd[2], int client_write_pipe_fd[2])
 {
     char mesg[BUFSIZ]; //메시지 읽는거
     ssize_t n,client_n;
--- chat server/clientmanager.h
+++ chat server/clientmanager.h
@@ -8,6 +8,6 @@
 
 #include "common.h"
 
-void client_work(pid_t client_server_pid,pid_t main_server_pid,int client_sock_fd, int client_read_pipe_fd, int client_write_pipe_fd);
+void client_work(pid_t client_server_pid,pid_t main_server_pid,int client_sock_fd, int client_read_pipe_fd[2], int client_write_pipe_fd[2]);
 
 #endif //CLIENTMANAGER_H
