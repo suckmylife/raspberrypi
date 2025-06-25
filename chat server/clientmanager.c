19b5064b67acb1709f8c0172307d69b7daeb3efd
kosa <thddpfla1107@gmail.com>
2025-06-25 18:14:10 +0900

    client blocking position change

--- chat server/clientmanager.c
+++ chat server/clientmanager.c
@@ -8,8 +8,9 @@ void client_work(pid_t client_server_pid,pid_t main_server_pid, int client_sock_
     close(client_to_main_pipe_fds[0]);
     while(!is_shutdown)
     {
-
+        set_nonblocking(client_sock_fd);
         n = recv(client_sock_fd, mesg, BUFSIZ-1, 0);
+        set_blocking(client_sock_fd);
         //클라이언트에서 메시지를 받는다
         if(n > 0){
             char add_mesg[BUFSIZ];
@@ -25,13 +26,13 @@ void client_work(pid_t client_server_pid,pid_t main_server_pid, int client_sock_
             //     syslog(LOG_INFO, "client_to_main_pipe_fds[1] is valid. Flags: %d", flags);
             // }
             ////test
-
+            set_nonblocking(client_to_main_pipe_fds[1]);
             ssize_t wlen = write(client_to_main_pipe_fds[1], add_mesg, strlen(add_mesg)+1);
             //syslog(LOG_INFO,"Received Client data : %s",add_mesg);
             if(kill(main_server_pid,SIG_FROM_CLIENT) == -1){
                     syslog(LOG_ERR, "NO KILL");
             }
-            set_nonblocking(is_write_from_client);
+            
             if(wlen <= 0){//클라이언트에게 받은걸 부모에게 쓴다.
                 syslog(LOG_ERR,"cannot Write to parent");
             } else if (errno == EAGAIN || errno == EWOULDBLOCK) {
