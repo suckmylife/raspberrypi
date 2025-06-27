5ae3f1a9c3074ecb44d17d5fdc5f068194d4dd95
kosa <thddpfla1107@gmail.com>
2025-06-27 14:34:10 +0900

    add sig in clientprocess

--- chat server/clientprocess.h
+++ chat server/clientprocess.h
@@ -2,7 +2,7 @@
 #define CLIENTPROCESS_H
 
 #include "comm.h"
-
+#include "sig.h"
 void client_work(pid_t client_pid, pid_t main_pid, \
                  int csock, int parent_pfd[2], int child_pfd[2]);
 
