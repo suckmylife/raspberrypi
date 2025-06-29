47f37a451abd2477330d6bbed00fcf11104f622e
newbie <thddpfla1107@gmail.com>
2025-06-30 03:33:46 +0900

    how to use snprintf

--- chat server/server.c
+++ chat server/server.c
@@ -284,7 +284,7 @@ int main(int argc, char **argv)
                                         if(strcmp(active_children[k].name, user_name) == 0){
                                             
                                             char final_message[BUFSIZ];
-                                            snprintf(final_message, "from %s : %s", active_children[client_idx].name, mesg);
+                                            snprintf(final_message, sizeof(final_message), "from %s : %s", active_children[client_idx].name, mesg);
                                             size_t name_len = strnlen(final_message, sizeof(final_message));
                                             ssize_t wlen = write(active_children[client_idx].parent_to_child_write_fd, final_message, name_len);
                                             if ( wlen <= 0) { 
