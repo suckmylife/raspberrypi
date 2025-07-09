c7aa89bfca6cbf90a8e4436d3fd4fa43a7fc130c
kosa <thddpfla1107@gmail.com>
2025-07-09 17:16:27 +0900

    wavplay

--- wavPlayer/wavplay.c
+++ wavPlayer/wavplay.c
@@ -9,101 +9,114 @@
 #include <sys/soundcard.h>
 
 #include "wavFile.h"
-
+WAVHEADER wavHeader;
 #define	BUF_SIZE	1024
 #define	AUDIO_DEV	"/dev/dsp"
+#define ALSA_PCM_NEW_HW_PARAMS_API
 
 int main(int argc, char** argv)
 {
-	int fd = -1, fd_sd = -1;
-	long count;
-	unsigned char data[BUF_SIZE];
+	int fd = -1;
+	int rc, buf_size, dir;
+	long count, remain;
+	unsigned int val;
+	char *buffer;
 	int channels;
 	int format;
-	wavHeader wavheader;
 
-	// Initialize Header of Wave
-	memset(&wavheader, 0, sizeof(wavHeader));
+	snd_pcm_t *handle;
+	snd_pcm_hw_params_t *params;
+	snd_pcm_uframes_t frames;
 
 	if(argc <= 1) {
 		printf("usage : %s filename\n", argv[0]);
-		exit(-1);
+		return -1;
 	} else {
 		printf("Playing file : %s\n", argv[1]);
 	}
 
 	if((fd = open(argv[1], O_RDONLY)) == -1) {
 		printf("Could not open the specified wave file : %s\n", argv[1]);
-		exit(-1);
+		return -1;
 	}
 
 	// Read Head of wave file
-	if((count = read(fd, &wavheader, sizeof(wavHeader))) < 1) {
+	if((count = read(fd, &wavheader, sizeof(WAVHEADER))) < 1) {
 		printf("Could not read wave data\n");
-		exit(-1);
+		goto END;
 	}
-
-	if((fd_sd = open(AUDIO_DEV, O_WRONLY)) == -1) {
-		printf("Error : Cound not open the audio device\n");
-		exit(-1);
+	rc = snd_pcm_open(&handle,"default",SND_PCM_STREAM_PLAYBACK,0);
+	if(rc < 0) {
+		fprintf(stderr,"unable to open pcm device : %s\n",snd_strerror(rc));
+		return -1;
 	}
-	
-	channels = wavheader.nChannels - 1;
+	snd_pcm_hw_params_alloca(&params);
+	snd_pcm_hw_params_any(handle,params);
+	channels = wavheader.nChannels;
 	printf("Wave Channel Mode : %s\n", (channels)? "Stereo":"Mono");
+	snd_pcm_hw_params_set_channels(handle,params,channels);
 
-	if((ioctl(fd_sd, /* fill the blank */, &channels)) == -1) {
-		printf("Error : Cound not set Channels\n");
-		exit(-1);
-	} else {
-		printf("Channel set to %s\n", (channels)? "Stereo":"Mono");
+	printf("Wave Bytes : %d\n",wavHeader.nblockAlign);
+	switch (wavHeader.nblockAlign)
+	{
+	case 1:
+		format = SND_PCM_FORMAT_U8;
+		break;
+	case 2:
+		format = (channels==1) ? SND_PCM_FORMAT_S16_LE : SND_PCM_FORMAT_U8;
+		break;
+	case 4:
+		format = SND_PCM_FORMAT_S16_LE;
+		break;
+	default:
+		printf("Unknown byte rate for sound\n");
+		return -1;
 	}
 
-	printf("Wave Bytes : %d\n", wavheader.nblockAlign);
-	switch(wavheader.nblockAlign) {
-		case 1:
-			format = AFMT_U8;
-			break;
-		case 2:
-			format = (!channels)?AFMT_S16_LE:AFMT_U8;
-			break;
-		case 4:
-			printf("Stereo Wave file\n");
-			format = AFMT_S16_LE;
-			break;
-		default:
-			printf("Unknown byte rate for sound\n");
-			break;
-	};
-	
-	if((ioctl(fd_sd, /* fill the blank */, &format)) == -1) {
-		printf("Error : Cound not set format\n");
-		exit(-1);
-	} else {
-		printf("Format set to %d\n", format);
+	snd_pcm_hw_params_set_access(handle,params,SND_PCM_ACCESS_RW_INTERLEAVED);
+	snd_pcm_hw_params_set_format(handle,params,format);
+
+	printf("Wave Sampling Rate : 0x%d\n",wavHeader.sampleRate);
+	val = wavHeader.sampleRate;
+	snd_pcm_hw_params_set_rate_near(handle,params,&val,&dir);
+
+	frames = 32;
+	snd_pcm_hw_params_set_period_size_near(handle,params,&frames,&dir);
+
+	rc = snd_pcm_hw_params(handle,params);
+	if(rc < 0){
+		fprintf(strerror, "Unable to set hw parameters : %s\n",snd_strerror(rc));
+		goto END;
 	}
 
-	printf("Wave Sampling Rate : 0x%d\n", wavheader.sampleRate);
-	if((ioctl(fd_sd, /* fill the blank */, &wavheader.sampleRate)) == -1) {
-		close(fd_sd);
-		exit(-1);
-	} else {
-		printf("Speed rate set to %d\n", wavheader.sampleRate);
-	}
+	snd_pcm_hw_params_get_period_size(params,&frames,&dir);
+	buf_size = frames * channels * ((format == SND_PCM_FORMAT_S16_LE)?2:1);
+	buffer = (char*)malloc(buf_size);
+
+	snd_pcm_hw_params_get_period_time(params,&val,&dir);
+	remain = wavHeader.dataLen;
 
 	do {
-		memset(data, 0, BUF_SIZE);
-		if((count = read(fd, data, BUF_SIZE)) <= 0) break;
-		if(write(fd_sd, data, count) == -1) {
+		buf_size = (remain > buf_size)?buf_size:remain;
+		if((count = read(fd, buffer, buf_size)) <= 0) break;
+		remain -= count;
+		rc = snd_pcm_writei(handle,buffer,frames);
+		if(rc == -EPIPE) {
 			printf("Error : Could not write to Sound Device\n");
-			exit(-1);
-		} else {
-			printf("Play Sound : %d bytes\n", count);
+			snd_pcm_prepare(handle);
+		} else if(rc <0){
+			fprintf(stderr,"error from write : %s\n",snd_strerror(rc));
+		}else{
+			fprintf(stderr,"short write, write : %d frames\n",rc);
 		}
-	} while(count == BUF_SIZE);
+	} while(count == buf_size);
 
-end:
+END:
 	close(fd);
-	close(fd_sd);
+	sleep(1);
+	snd_pcm_drain(handle);
+	snd_pcm_close(handle);
+	free(buffer);
 
 	return 0;
 }
