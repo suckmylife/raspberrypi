2fe5adde180b2954816fa3d91d06fe41e8941b31
kosa <thddpfla1107@gmail.com>
2025-07-10 14:26:17 +0900

    face

--- 0710/fbdraw.c
+++ 0710/fbdraw.c
@@ -84,7 +84,7 @@ static void drawface(int fd, int start_x,int start_y,int end_x,int end_y,ubyte r
     ubyte a = 0xFF;
     if(end_x == 0) end_x = vinfo.xres;
     if(end_y == 0) end_x = vinfo.yres;
-
+    unsigned short pixel_color = makepixel(r, g, b);
     for(int x = start_x; x < end_x; x++){
         for(int y = start_y; y<end_y; y++){
             int offset =  (x + y * vinfo.xres) * 2;
