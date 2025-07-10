82f8ce4ebf66987122281bf83d2905ee4b881706
kosa <thddpfla1107@gmail.com>
2025-07-10 15:06:44 +0900

    face

--- 0710/fbdraw.c
+++ 0710/fbdraw.c
@@ -109,14 +109,15 @@ int main(int argc, char **argv)
         return -1;
     }
     //면 그리기
-    drawface(fbfd, 0,0,100,200,0,0,255);
-    drawface(fbfd, 100,0,200,200,255,255,255);
-    drawface(fbfd, 200,0,300,200,255,0,0);
+    drawface(fbfd, 0,0,0,0,255,255,255);
+    drawface(fbfd, 0,0,500,800,0,0,255);
+    
+    drawface(fbfd, 1000,0,1300,800,255,0,0);
     //drawface(fbfd, 0,100,100,300,0,0,255);
     // (100,200)에서 (300, 150)까지 초록색 선 그리기
-    drawline(fbfd, 100, 200, 300, 150, 0, 255, 0);
+    //drawline(fbfd, 100, 200, 300, 150, 0, 255, 0);
     //원그리기
-    drawcircle(fbfd, 200,200,100,255,0,255);
+    //drawcircle(fbfd, 200,200,100,255,0,255);
     
 
     close(fbfd);
