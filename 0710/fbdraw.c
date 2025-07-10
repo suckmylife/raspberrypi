addf856b956971cafef8ac095e5e21226244866b
kosa <thddpfla1107@gmail.com>
2025-07-10 15:37:13 +0900

    face

--- 0710/fbdraw.c
+++ 0710/fbdraw.c
@@ -118,6 +118,7 @@ int main(int argc, char **argv)
     drawline(fbfd, 600,100,400,400,255,0,0);
     drawline(fbfd, 600,100,400,400,255,0,0);
     drawline(fbfd, 400,400,370,360,255,0,0);
+    drawline(fbfd, 370,360,410,200,255,0,0);
     
     //drawface(fbfd, 0,100,100,300,0,0,255);
     // (100,200)에서 (300, 150)까지 초록색 선 그리기
