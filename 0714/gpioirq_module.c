c5db5abb80c103f76a10d4f821a6102dc602bc2b
kosa <thddpfla1107@gmail.com>
2025-07-18 09:35:05 +0900

    irq modi

--- 0714/gpioirq_module.c
+++ 0714/gpioirq_module.c
@@ -10,23 +10,6 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("SYL");
 MODULE_DESCRIPTION("Rasberry Pi GPIO LED Device Module");
-#if 0
-//#define BCM_IO_BASE 0x20000000    /* Raspberry Pi B/B+의 I/O Peripherals 주소 */
-#define BCM_IO_BASE  0x3F000000 /* Raspberry Pi 2/3의 I/O Peripherals 주소 */
-#else
-#define BCM_IO_BASE 0xFE000000 /* Raspberry Pi 4의 I/O Peripherals 주소 */
-#endif
-#define GPIO_BASE (BCM_IO_BASE + 0x200000)
-#define GPIO_SIZE  (256) /* GPIO 컨트롤러의 주소 */
-/* 0x7E2000B0 – 0x7E2000000 + 4 = 176 + 4 = 180 */
-
-/* GPIO 설정 매크로 */
-#define GPIO_IN(g) (*(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))) /* 입력 설정 */
-#define GPIO_OUT(g) (*(gpio+((g)/10)) |= (1<<(((g)%10)*3))) /* 출력 설정 */
-
-#define GPIO_SET(g) (*(gpio+7) = 1<<g) /* 비트 설정 */
-#define GPIO_CLR(g) (*(gpio+10) = 1<<g) /* 설정된 비트 해제 */
-#define GPIO_GET(g) (*(gpio+13)&(1<<g)) /* 현재 GPIO의 비트에 대한 정보 획득 */
 
 #define GPIO_MAJOR 200
 #define GPIO_MINOR 0
